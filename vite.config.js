7e51ecd63c5f11bdf3d06e16fed5f50a8011ecee
carina <115407229+t14c4r1n4@users.noreply.github.com>
2024-05-21 11:24:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
-// vite.config.js
-export default {
-  // config options
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
+  })
