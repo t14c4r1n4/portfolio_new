1af58e66ddbd47ef0fa201dcb039a64c3ce4c8e1
carina <115407229+t14c4r1n4@users.noreply.github.com>
2024-05-30 11:49:03 +0000

    deleted unused code

--- src/assets/components/navbar.jsx
+++ src/assets/components/navbar.jsx
@@ -1,46 +1,41 @@
-import React, { useRef, useState } from "react";
+import React, { createRef, useRef, useState } from "react";
 import nav_items from "../constants/nav_items";
 import ScrollSpy from "react-scrollspy-navigation";
 import scrollPosition from "react-scrollspy-navigation"
 
 const Navbar = () => {
   return (
-    <ScrollSpy activeClass="nav-active" scrollTargetIds={nav_items.map(item => item.id)}>
-      <div className="w-full fixed top-0 z-40 bg-zinc-900">
-          <nav className="w-screen h-[10vh]">
-            <ul className="flex flex-row place-content-center space-between">
-              {nav_items.map((page, index) => (
-                <li className="py-[2vh]" key={index}>
-                  {/* Mobile View */}
-                  <a href={`#${page.id}`}
-                    aria-label={`Scroll to ${page.id}`}
-                    className={`md:hidden relative flex hover:font-extrabold ${scrollPosition >= document.getElementById(page.id)?.offsetTop &&
-                        scrollPosition < document.getElementById(page.id)?.offsetTop + document.getElementById(page.id)?.offsetHeight
-                        ? "nav-active"
-                        : ""
-                      }`}
-                  >
-                    {page.icon}
-                  </a>
-                  {/* Display View */}
-                  <a
-                    href={`#${page.id}`}
-                    aria-label={`Scroll to ${page.id}`}
-                    className={`hidden md:inline-block relative flex hover:font-extrabold ${scrollPosition >= document.getElementById(page.id)?.offsetTop &&
-                        scrollPosition < document.getElementById(page.id)?.offsetTop + document.getElementById(page.id)?.offsetHeight
-                        ? "nav-active"
-                        : ""
-                      }`}
-                  >
-                    {page.name}
-                  </a>
-                  
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-    </ScrollSpy>
+
+    <div className="w-full fixed top-0 z-40 bg-zinc-900">
+      <nav className="w-screen h-[10vh]">
+
+        <ul className="flex flex-row place-content-center space-between">
+          <ScrollSpy activeClass="nav-active" scrollTargetIds={nav_items.map(item => item.id)}>
+            {nav_items.map((page, index) => (
+              <li className="py-[2vh]" key={index} ref={createRef()}>
+                {/* Mobile View - Icons*/}
+                <a href={`#${page.id}`}
+                  aria-label={`Scroll to ${page.id}`}
+                  className={`md:hidden relative flex hover:font-extrabold`}
+                >
+                  {page.icon}
+                </a>
+                {/* Display View - Text*/}
+                <a
+                  href={`#${page.id}`}
+                  aria-label={`Scroll to ${page.id}`}
+                  className={`hidden md:inline-block relative flex hover:font-extrabold `}
+                >
+                  {page.name}
+                </a>
+              </li>
+            ))}
+          </ScrollSpy>
+        </ul>
+
+      </nav>
+    </div>
+
   );
 };
 
